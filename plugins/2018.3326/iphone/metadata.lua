df5a230b4ec4634e505ce24ee8302d5cee987735
Scott Harrison <scottrules44@gmail.com>
2022-02-11 20:08:45 -0500

    Initial Commit

---
+++ plugins/2018.3326/iphone/metadata.lua
@@ -0,0 +1,14 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+
+		staticLibs = { "ObjC", "c++", "z"},
+		frameworks = {"AppLovinMediationUnityAdsAdapter", "UnityAds"},
+		frameworksOptional = {"AppTrackingTransparency"},
+	},
+
+}
+
+return metadata
