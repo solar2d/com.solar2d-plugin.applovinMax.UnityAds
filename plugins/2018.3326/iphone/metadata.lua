a663eb460326ea7674dedd457b817128a02e7b56
Scott Harrison <scottrules44@gmail.com>
2024-06-17 16:38:54 -0500

    Max updates 12.5

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -6,9 +6,9 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = {  "c++", "sqlite3", "z", "UnityAds", "UnityAdsAdapter"},
+		staticLibs = {  "c++", "sqlite3", "z", "UnityAdsAdapter"},
 
-		frameworks = {},
+		frameworks = {"UnityAds"},
 		frameworksOptional = {"AdSupport", "AppTrackingTransparency"},
 		usesSwift = true,
 	},
